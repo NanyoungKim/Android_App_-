93907a116e226c743713c54db4d904bdb04d2487
NanyoungKim <56589325+NanyoungKim@users.noreply.github.com>
2020-07-28 16:24:19 +0900

    Update Cat.java

--- AbstractClass/Cat.java
+++ AbstractClass/Cat.java
@@ -1,12 +1,18 @@
 
-public class Cat extends Animal{
+public class Cat extends Animal implements Pet {
  
- public Cat(String name) { super(name); }
+   public Cat(String name) { super(name); }
 
-@Override
- public void Cry(){
-   System.out.println(name + "~~~~");
- }
+  @Override
+   public void Cry(){
+     System.out.println(name + "~~~~");
+   }
+
+
+  @Override
+   public void FoodCall() {
+      System.out.println("......");
+   } 
  
 }
 
